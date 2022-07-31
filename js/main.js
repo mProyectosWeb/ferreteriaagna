35a4997ed581395eed811a5ec3f87fbcd269fca8
Miguel Rodriguez <m.angel.desarrolladorweb@gmail.com>
2022-07-31 00:47:06 -0500

    Dividimos los CSS y agregamos el responsive

--- js/main.js
+++ js/main.js
@@ -1,14 +1,9 @@
-let enlaces = document.querySelectorAll('.lists__ul a');
-enlaces.forEach((element) =>{
-    element.addEventListener('click', (event) =>{
-        enlaces.forEach((link) =>{
-            link.classList.remove('lists__a');
-        });
-        event.target.classList.add('lists__a');   
-    });
+let enlaces = document.querySelector(".menu__hamburguesa");
+let barIconX = document.querySelector('.menu__hamburguesa i');
+
+enlaces.addEventListener('click', (event) => {  
+    barIconX.classList.toggle('fa-times');
 });
-
-
 
 // Efecto ocultar y mostrar la barra de menu
 let menu = document.querySelector(".menu");
